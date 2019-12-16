bdfa717d5384cc8f6614250795156b3728acda46
Sergio Orellana <orellana123@gmail.com>
2019-12-15 23:46:07 -0300

    Up
    Update

--- peliculas_learning/lib/main.dart
+++ peliculas_learning/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:peliculas_learning/src/routes/routes.dart';
 
 
- 
+//Llamado inicio
 void main() => runApp(MyApp());
  
 class MyApp extends StatelessWidget {
